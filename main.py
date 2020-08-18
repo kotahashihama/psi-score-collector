6379755bcdb5e304ee316d2754a4216aba96a3e2
Kota Hashihama <kotahashihama@gmail.com>
2020-08-18 23:02:06 +0900

    feat: ラボデータの累積レイアウト変更を出力

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ def measure(device):
   field_lcp_scores = []
   field_fid_scores = []
   field_cls_scores = []
+  lab_cls_scores = []
 
   for i in range(measurement_count):
     result = requests.get(url_name, params = payload)
@@ -61,10 +62,14 @@ def measure(device):
     field_data_scores['cls'] = \
       result_json['loadingExperience']['metrics']['CUMULATIVE_LAYOUT_SHIFT_SCORE']['percentile']
 
+    lab_data_scores = {}
+    lab_data_scores['cls'] = result_json['lighthouseResult']['audits']['cumulative-layout-shift']['numericValue']
+
     field_fcp_scores.append(field_data_scores['fcp'])
     field_lcp_scores.append(field_data_scores['lcp'])
     field_fid_scores.append(field_data_scores['fid'])
     field_cls_scores.append(field_data_scores['cls'])
+    lab_cls_scores.append(lab_data_scores['cls'])
     scores.append(displayed_score)
     print(displayed_score, end=' ')
 
@@ -73,6 +78,7 @@ def measure(device):
   average_field_lcp_scores = calculate_average(field_lcp_scores)
   average_field_fid_scores = calculate_average(field_fid_scores)
   average_field_cls_scores = calculate_average(field_cls_scores)
+  average_lab_cls_scores = calculate_average(lab_cls_scores)
   print('\n平均 {main} 点（最低 {min} 点、最高 {max} 点）' \
     .format(
       main=average_scores['main'],
@@ -84,6 +90,8 @@ def measure(device):
   print('LCP: 平均 {main} s'.format(main=average_field_lcp_scores['main']), end=', ')
   print('FID: 平均 {main} ms'.format(main=average_field_fid_scores['main']), end=', ')
   print('CLS: 平均 {main}'.format(main=average_field_cls_scores['main']))
+  print('(ラボデータ)', end=' ')
+  print('累積レイアウト変更: 平均 {main}'.format(main=average_lab_cls_scores['main']))
 
 print('\n'.join(map(str, url_list)))
 print(f'を測定中...({measurement_count}回計測)')
