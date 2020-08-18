31a30dd84cf35c46352ac5667ce1bc408f7a8714
Kota Hashihama <kotahashihama@gmail.com>
2020-08-18 22:42:45 +0900

    feat: フィールドデータを出力

--- main.py
+++ main.py
@@ -19,6 +19,15 @@ api_key = config.API_KEY
 
 payload = { 'key': api_key }
 
+def calculate_average(scores):
+  score_average_raw = numpy.average(scores)
+  average_scores = {}
+  average_scores['main'] = Decimal(str(score_average_raw)).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)
+  average_scores['max'] = numpy.amax(scores)
+  average_scores['min'] = numpy.amin(scores)
+
+  return average_scores
+
 def measure(device):
   device_name = {
     'mobile': 'モバイル',
@@ -31,6 +40,10 @@ def measure(device):
   url_name = api_url + "?url=" + url
 
   scores = []
+  field_fcp_scores = []
+  field_lcp_scores = []
+  field_fid_scores = []
+  field_cls_scores = []
 
   for i in range(measurement_count):
     result = requests.get(url_name, params = payload)
@@ -38,14 +51,39 @@ def measure(device):
     result_score = result_json['lighthouseResult']['categories']['performance']['score']
     displayed_score = math.floor(result_score * 100)
 
+    field_data_scores = {}
+    field_data_scores['fcp'] = \
+      result_json['loadingExperience']['metrics']['FIRST_CONTENTFUL_PAINT_MS']['percentile'] / 1000
+    field_data_scores['lcp'] = \
+      result_json['loadingExperience']['metrics']['LARGEST_CONTENTFUL_PAINT_MS']['percentile'] / 1000
+    field_data_scores['fid'] = \
+      result_json['loadingExperience']['metrics']['FIRST_INPUT_DELAY_MS']['percentile']
+    field_data_scores['cls'] = \
+      result_json['loadingExperience']['metrics']['CUMULATIVE_LAYOUT_SHIFT_SCORE']['percentile']
+
+    field_fcp_scores.append(field_data_scores['fcp'])
+    field_lcp_scores.append(field_data_scores['lcp'])
+    field_fid_scores.append(field_data_scores['fid'])
+    field_cls_scores.append(field_data_scores['cls'])
     scores.append(displayed_score)
     print(displayed_score, end=' ')
 
-  score_average_raw = numpy.average(scores)
-  score_average = Decimal(str(score_average_raw)).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP)
-  score_max = numpy.amax(scores)
-  score_min = numpy.amin(scores)
-  print(f'\n平均 {score_average} 点（最低 {score_min} 点、最高 {score_max} 点）')
+  average_scores = calculate_average(scores)
+  average_field_fcp_scores = calculate_average(field_fcp_scores)
+  average_field_lcp_scores = calculate_average(field_lcp_scores)
+  average_field_fid_scores = calculate_average(field_fid_scores)
+  average_field_cls_scores = calculate_average(field_cls_scores)
+  print('\n平均 {main} 点（最低 {min} 点、最高 {max} 点）' \
+    .format(
+      main=average_scores['main'],
+      min=average_scores['min'],
+      max=average_scores['max']
+    ))
+  print('(フィールドデータ)', end=' ')
+  print('FCP: 平均 {main} s'.format(main=average_field_fcp_scores['main']), end=', ')
+  print('LCP: 平均 {main} s'.format(main=average_field_lcp_scores['main']), end=', ')
+  print('FID: 平均 {main} ms'.format(main=average_field_fid_scores['main']), end=', ')
+  print('CLS: 平均 {main}'.format(main=average_field_cls_scores['main']))
 
 print('\n'.join(map(str, url_list)))
 print(f'を測定中...({measurement_count}回計測)')
